dba7a6dcef7f74e327d0cb333a7e04af16df8c3f
Gurzhiy <a.j.lapin1@gmail.com>
2020-11-29 05:29:54 +1000

    all code and tests

---
+++ src/main/java/Main.java
@@ -0,0 +1,55 @@
+import com.sun.javafx.binding.StringFormatter;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Main {
+
+
+    /**
+     * @return result of equation a * (b + c / d), where a,b,c,d double numbers
+     */
+    public double getEquation(double a, double b, double c, double d) {
+        return a * (b + c / d);
+    }
+
+    /**
+     * @param a int
+     * @param b int
+     * @return true if sum of a and b in the set of [10, 20], false if not in it
+     */
+    public boolean sum(int a, int b) {
+        return (a + b) <= 20 && (a + b) >= 10 ? true : false;
+    }
+
+    public void printSign(int number) {
+        String sign = number >= 0 ? "positive" : "negative";
+        System.out.printf("This is %s number.\n", sign);
+    }
+
+
+    public boolean isNegative(int number) {
+        return number < 0 ? true : false;
+    }
+
+    public void greeter(String name) {
+        String output = String.format("Hello %s", name);
+        System.out.print(output);
+    }
+
+    //check leap year by Date Time API
+    public boolean isLeapYear(int year) {
+        LocalDate someWhen = LocalDate.ofYearDay(year, 1);
+        return someWhen.isLeapYear();
+    }
+
+
+    public boolean isLeapYearBeforeLocalDate(int year) {
+        boolean isLeap = false;
+        if (year % 4 == 0) isLeap = true;
+        if (year % 100 == 0) isLeap = false;
+        if (year % 400 == 0) isLeap = true;
+        return isLeap;
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,100 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    private final Main myMain = new Main();
+    private final ByteArrayOutputStream outForTests = new ByteArrayOutputStream();
+    private final PrintStream originalOutput = System.out;
+
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outForTests));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOutput);
+    }
+
+
+    // a * (b + c / d);
+    @Test
+    public void testEq() {
+        assertEquals(myMain.getEquation(1, 2, 4, 2), 4, 0.1);
+        assertEquals(myMain.getEquation(1, 2, 3, 2), 3.5, 0.1);
+        assertEquals(myMain.getEquation(1, 1, 3, 2), 2.5, 0.1);
+    }
+
+    @Test
+    public void testSum() {
+        assertEquals(myMain.sum(1, 2), false);
+        assertEquals(myMain.sum(10, 0), true);
+        assertEquals(myMain.sum(20, 0), true);
+        assertEquals(myMain.sum(21, 0), false);
+    }
+
+
+    @Test
+    public void testIsNegative() {
+        assertEquals(myMain.isNegative(1), false);
+        assertEquals(myMain.isNegative(-1), true);
+        assertEquals(myMain.isNegative(0), false);
+    }
+
+    @Test
+    public void outputTest() {
+        String message = "hello, this is test.";
+        System.out.print(message);
+        assertEquals(message, outForTests.toString());
+    }
+
+    @Test
+    public void testPrintSignPos() {
+        myMain.printSign(1);
+        assertEquals(outForTests.toString(), "This is positive number.\n");
+    }
+
+    @Test
+    public void testPrintSignNeg() {
+        myMain.printSign(-1);
+        assertEquals(outForTests.toString(), "This is negative number.\n");
+
+    }
+
+    @Test
+    public void testPrintSignZero() {
+        myMain.printSign(0);
+        assertEquals(outForTests.toString(), "This is positive number.\n");
+
+    }
+
+
+    @Test
+    public void testGreeter() {
+        myMain.greeter("Tony Stark");
+        assertEquals("Hello Tony Stark", outForTests.toString());
+    }
+
+    @Test
+    public void checkIsLeapYear(){
+        assertEquals(myMain.isLeapYear(2112), true);
+        assertEquals(myMain.isLeapYear(2113), false);
+        assertEquals(myMain.isLeapYear(2116), true);
+    }
+
+    @Test
+    public void checkIsLeapYearBeforeLocalDate(){
+        for (int year = 0; year < 2000; year++) {
+            assertEquals(myMain.isLeapYearBeforeLocalDate(year), myMain.isLeapYear(year));
+        }
+    }
+
+
+}
